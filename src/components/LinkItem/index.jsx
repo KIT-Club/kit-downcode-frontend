5c8e6daaf8a20de8d88bc5ff5f022d73be027c7f
arahiko-ayami <zinang7@gmail.com>
2022-01-13 15:46:00 +0700

    add: handle copy code from LinkItem

--- src/components/LinkItem/index.jsx
+++ src/components/LinkItem/index.jsx
@@ -11,16 +11,31 @@ function LinkItem({ code, link }) {
     context.remove({ code, link });
   };
 
+  const handleOnClickClipboard = async () => {
+    // Using clipboard API
+    if (navigator.clipboard) await navigator.clipboard.writeText(code);
+    // Fallback function using old method
+    else {
+      const codeElement = document.getElementById('code');
+      const input = document.createElement('textarea');
+      input.value = codeElement.textContent;
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand('copy');
+      input.remove();
+    }
+  };
+
   return (
     <div className="w-full flex flex-col items-center">
       <div className="w-72 mx-2 h-12 rounded-lg dark:bg-gray-5 bg-gray-1 flex justify-center items-center md:w-96 2xl:w-1/4 max-w-2xl pl-5">
-        <p
+        <div
           className="justify-center rounded-lg dark:bg-gray-5 bg-gray-1 text-sm outline-none w-full dark:text-gray-2 text-gray-5 whitespace-nowrap overflow-x-auto"
         >
-          {code === '' ? '' : <p className="text-lg dark:text-gray-2 text-gray-5 text-opacity-70 leading-4 font-bold">{code}</p>}
+          {code === '' ? '' : <p id="code" className="text-lg dark:text-gray-2 text-gray-5 text-opacity-70 leading-4 font-bold">{code}</p>}
           {link}
-        </p>
-        <button className="px-5" type="button" onClick={handleOnClickDelete}>
+        </div>
+        <button className="px-5" type="button" onClick={code === '' ? handleOnClickDelete : handleOnClickClipboard}>
           <div style={{ width: '24px', height: '24px' }}>
             {code === '' ? <TrashIcon /> : <Clipboard />}
           </div>
