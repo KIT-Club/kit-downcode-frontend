f872730a21771c637e9524f33f8b03cf05ec3806
arahiko-ayami <zinang7@gmail.com>
2022-01-17 20:03:17 +0700

    add: close icon

---
+++ src/assets/icons/Close.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Icon() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      className="stroke-current"
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6L18 18" />
+    </svg>
+  );
+}
+
+export default Icon;
